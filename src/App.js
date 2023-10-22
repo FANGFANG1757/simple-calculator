50794d2df12667f75d4149e9590681c779d72962
Zhongyuan Zhang <zhongyuan.zhang@booking.com>
2023-10-22 23:59:43 +0800

    feat: refactor calc logic

--- src/App.js
+++ src/App.js
@@ -1,70 +1,58 @@
-import { useState, useRef } from "react";
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const inputRef = useRef(null);
-  const resultRef = useRef(null);
-  const [result, setResult] = useState(0);
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState(null);
 
-  function plus(e) {
-    e.preventDefault();
-    setResult((result) => result + Number(inputRef.current.value));
-  }
-
-  function minus(e) {
-    // Add the code for the minus function
-    e.preventDefault();
-    setResult((result) => result - Number(inputRef.current.value));
-  }
-
-  function times(e) {
-    // Add the code for the plus function
-    e.preventDefault();
-    setResult((result) => result * Number(inputRef.current.value));
-  }
-
-  function divide(e) {
-    // Add the code for the divide function
-    e.preventDefault();
-    setResult((result) => result / Number(inputRef.current.value));
-  }
-
-  function resetInput(e) {
-    // Add the code for the resetInput function
-    e.preventDefault();
-    inputRef.current.value = "";
+  function resetInput() {
+    setInput('');
   }
 
   function resetResult(e) {
-    // Add the code for the resetResult function
     e.preventDefault();
-    setResult(0);
-    inputRef.current.value = "";
+    setResult(null);
   }
 
+  function handleCalclate(calcFn, e) {
+    e.preventDefault();
+    setResult((result) =>
+      result === null ? Number(input) : calcFn(result, Number(input)),
+    );
+    resetInput();
+  }
   return (
     <div className="App">
       <div>
         <h1>Simplest Working Calculator</h1>
       </div>
       <form>
-        <p ref={resultRef}>{/* add the value of the current total */ result}</p>
+        <p>{result ?? 'Please input your number'}</p>
         <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
           pattern="[0-9]"
-          ref={inputRef}
           type="number"
           placeholder="Type a number"
         />
-        <button onClick={plus}>add</button>
-        {/* Add the subtract button */}
-        <button onClick={minus}>subtract</button>
-        {/* Add the multiply button */}
-        <button onClick={times}>multiply</button>
-        {/* Add the divide button */}
-        <button onClick={divide}>divide</button>
-        {/* Add the resetInput button */}
+        <button onClick={(e) => handleCalclate((a, b) => a + b, e)}>add</button>
+        <button onClick={(e) => handleCalclate((a, b) => a - b, e)}>
+          subtract
+        </button>
+        <button onClick={(e) => handleCalclate((a, b) => a * b, e)}>
+          multiply
+        </button>
+        <button
+          onClick={(e) =>
+            handleCalclate(
+              (a, b) => (b === 0 ? 'ERROR: COULD DIVIDE ZERO' : a / b),
+              e,
+            )
+          }
+        >
+          divide
+        </button>
         <button onClick={resetInput}>reset input</button>
-        {/* Add the resetResult button */}
         <button onClick={resetResult}>reset result</button>
       </form>
     </div>
